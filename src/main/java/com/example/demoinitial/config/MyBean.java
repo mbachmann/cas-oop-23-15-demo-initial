44aaab4ff51dc97714b82dd57ecd9b295746d346
mbachmann <m.bachmann@united.ch>
2023-05-15 13:46:05 +0200

    initial - after LE05

--- src/main/java/com/example/demoinitial/config/MyBean.java
+++ src/main/java/com/example/demoinitial/config/MyBean.java
@@ -5,12 +5,13 @@ import com.example.demoinitial.utils.HasLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
-@Component
+@Configuration
 public class MyBean implements CommandLineRunner, HasLogger {
     MyComponent myComponent;
 
@@ -18,13 +19,12 @@ public class MyBean implements CommandLineRunner, HasLogger {
     @Named("felixMuster")
     Person felixMuster;
 
-    @Autowired
-    @Qualifier("maxMustermann")
     Person maxMustermann;
 
     @Autowired
-    public MyBean(MyComponent myComponent) {
+    public MyBean(MyComponent myComponent, @Qualifier("maxMustermann") Person maxMustermann) {
         this.myComponent = myComponent;
+        this.maxMustermann = maxMustermann;
     }
 
     public void run(String[] args) {
